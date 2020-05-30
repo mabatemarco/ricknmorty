90b2b18349f1559abeaf387b7eacc5ffcab18bda
mabatemarco <mabatemarco928@gmail.com>
2020-05-30 16:51:53 -0400

    fixed more characters button

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 
 export default function Characters(props) {
   return (
-    <div className="characters">
-      
-      {props.charactersArray && props.charactersArray.map(character => (
-        <Link to={`/character/${character.name}`}>
-          <div className="character">
-            <img src={character.image} alt="" />
-            <h3>{character.name}</h3>
-          </div>
-        </Link>
-      ))}
-      <button onClick={props.getCharacters}>Get More Characters</button>
-    </div>
+    <>
+      <div className="characters">
+
+        {props.charactersArray && props.charactersArray.map(character => (
+          <Link to={`/character/${character.name}`}>
+            <div className="character">
+              <img src={character.image} alt="" />
+              <h3>{character.name}</h3>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <button className='moreCharacters' onClick={props.getCharacters}>Get More Characters</button>
+    </>
   )
 }
